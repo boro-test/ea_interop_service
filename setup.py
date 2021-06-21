e895da121c425f8c20f6061ce5796da08eefed35
xibertao <xibertao@borogroup.co.uk>
2021-06-21 10:59:01 +0100

    Drop 2 commit.

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(
     name='ea_interop_service',
-    version='0.1.0-alpha',
+    version='0.1.0-alpha.1',
     description='EA Interop Service',
     author='BORO Solutions',
     author_email='xibertao@borogroup.co.uk',
-    url='https://github.com/boro-test/ea_interop_service.git',
+    url='https://github.com/boro-alpha/ea_interop_service.git',
     packages=setuptools.find_packages(),
     include_package_data=True)
